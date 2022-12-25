2aecad7becb8f5a295fac7aec7762f4ae2f9db69
Saurabh Saxena <sausax@gmail.com>
2022-12-25 10:07:22 -0800

    Update lpw_stable_diffusion.py

--- examples/community/lpw_stable_diffusion.py
+++ examples/community/lpw_stable_diffusion.py
@@ -587,6 +587,7 @@ class StableDiffusionLongPromptWeightingPipeline(StableDiffusionPipeline):
             return timesteps, num_inference_steps - t_start
 
     def run_safety_checker(self, image, device, dtype):
+        return image, None
         if self.safety_checker is not None:
             safety_checker_input = self.feature_extractor(self.numpy_to_pil(image), return_tensors="pt").to(device)
             image, has_nsfw_concept = self.safety_checker(
